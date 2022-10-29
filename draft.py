53dfc01c553b40549e922c4b032b900dc0233e27
MuhammedMiiRanna <merali.med@gmail.com>
2022-10-29 22:56:39 +0100

    update .gitignore + xlsx file

---
+++ draft.py
@@ -0,0 +1,24 @@
+# 10/2/2022 21:01:53
+# https://myaccount.google.com/apppasswords?rapt=AEjHL4MfNrq7onFGWSIELHL9B7cQHFwBJOmP1Tk4aLi1nBZM7BUYmQ3EEZOPCw93queTgJTp-Nkg1hD7_64dNZcsHS_TWQLYPQ
+
+# participantes_file_path = "participante.xlsx"
+# participantes_file = open(participantes_file_path)
+# participantes_csv_file = csv.reader(participantes_file)
+
+# rows = []
+# for row in participantes_csv_file:
+#     rows.append(row)
+
+# #################################################################
+
+# # Define variable to load the dataframe
+# dataframe = openpyxl.load_workbook("participante.xlsx")
+
+# # Define variable to read sheet
+# dataframe1 = dataframe.active
+
+# # Iterate the loop to read the cell values
+# for row in range(0, dataframe1.max_row):
+#     for col in dataframe1.iter_cols(1, dataframe1.max_column):
+#         print(col[row].value)
+# #################################################################
