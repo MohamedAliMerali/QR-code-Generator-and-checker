cecfc75aa17d4f5ea1a1c952f4e213a34ba66f6e
MuhammedMiiRanna <merali.med@gmail.com>
2022-11-07 18:01:13 +0100

    alpha version ready

--- QRCodeChecker.py
+++ QRCodeChecker.py
@@ -1,5 +1,6 @@
-import cv2 as cv
 import json
+import cv2 as cv
+from datetime import datetime
 
 # TODO: check if the file exist
 with open("participantes_data.json", "r") as data_file:
@@ -18,13 +19,25 @@ while True:
     # check if there is a QRCode in the image
     if data:
         prtcp_data = data.split("-")
-        prtcp_info = json_data.get(prtcp_data[0], [0])
-        prtcp_info[0] += 1
+        prtcp_info = json_data.get(prtcp_data[0], 0)
+        if not prtcp_info:
+            print('>> PARTICIPANT DO NOT EXIST !!')
+            continue
+        # prtcp_info[0] += 1
+        if len(prtcp_info[4]) == 0:
+            prtcp_info[4].append(str(datetime.now()))
+        elif int(prtcp_info[4][-1].split(" ")[0].split("-")[2]) == datetime.now().day:
+               print(">> PARTICIPANT ALREADY CHECKED IN !!")
+        else:
+            prtcp_info[4].append(str(datetime.now()))
         print(data)
 
     cv.imshow("QRCODEscanner", img)
     if cv.waitKey(1) == ord("q"):
         break
+
+with open("participantes_data.json", "w") as data_file:
+    data_file.write(json.dumps(json_data, indent=4))
 
 print(json_data["1004220138"])
 cap.release()
--- QRCodeGenerator.py
+++ QRCodeGenerator.py
@@ -3,38 +3,28 @@ import json
 import qrcode
 import smtplib
 import openpyxl
+from lib import *
 from os import system
+from time import time, strftime, gmtime
 from email.message import EmailMessage
 
+begin_time = time()
 system('cls')
 print('==> {:.2f} %'.format(0))
 
-
-def tstamp_to_id(tstamp):
-    date, time = tstamp.split(" ")
-    date = date.split("-")
-    time = time.split(":")
-    date.pop(0)
-    date.extend(time)
-    return "".join(date)
-
-
-excel_path = "participante.xlsx"
-with open("apppassword.txt", "r") as apppass:
-    app_pass = apppass.readline().strip()
-
-# Define email sender and receiver
-email_sender = 'merali.med@gmail.com'
-email_password = app_pass
-# Set the subject and body of the email (event name for exp)
-subject = 'The Purge has began!'
+json_data = {}
+with open("settings.json", "r") as data_file:
+    settings = json.load(data_file)
+with open(settings["app_password_file"], "r") as apppass:
+    email_password = apppass.readline().strip()
 
 # To open the workbook workbook object is created
-wb_obj = openpyxl.load_workbook(excel_path)
+wb_obj = openpyxl.load_workbook(settings["excel_file"])
 # Get workbook active sheet object from the active attribute
 sheet = wb_obj.active
 
-json_data = {}
+# Set the subject and body of the email (event name for exp)
+subject = 'The Purge has began!'
 
 for row_id in range(2, sheet.max_row+1):
     id_cell = sheet.cell(row=row_id, column=1).value
@@ -42,8 +32,9 @@ for row_id in range(2, sheet.max_row+1):
     prtc_first_name = sheet.cell(row=row_id, column=2).value
     prtc_last_name = sheet.cell(row=row_id, column=3).value
     prtc_email = sheet.cell(row=row_id, column=4).value
+    # prtc_number = sheet.cell(row=row_id, column=5).value
     json_data[prtc_id] = [prtc_first_name,
-                          prtc_last_name, prtc_email, id_cell, []]
+                          prtc_last_name, prtc_email, prtc_id, []]
 
     # Data to be encoded
     # data = 'QR Code using make() function'
@@ -60,7 +51,7 @@ for row_id in range(2, sheet.max_row+1):
     we hope that all is good
     
     that's a test of the auto emails script
-    in the next email you will receive your QR code
+    your QR code is attached?
     
     
     Best regards,
@@ -68,7 +59,7 @@ for row_id in range(2, sheet.max_row+1):
     """
     email_receiver = prtc_email
     em = EmailMessage()
-    em['From'] = email_sender
+    em['From'] = settings["email_sender"]
     em['To'] = email_receiver
     em['Subject'] = subject
     em.set_content(body)
@@ -76,19 +67,20 @@ for row_id in range(2, sheet.max_row+1):
         img_data = fp.read()
     em.add_attachment(img_data, maintype='image', subtype='png')
 
-    # Sending the email:
-    # Add SSL (layer of security)
+    # Sending the email: Add SSL (layer of security)
     context = ssl.create_default_context()
     # Log in and send the email
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
+        smtp.login(settings["email_sender"], email_password)
+        smtp.sendmail(settings["email_sender"], email_receiver, em.as_string())
 
     system('cls')
     print('==> {:.2f} %'.format((row_id)*100/sheet.max_row))
 
 # TODO: check if the file exist
-with open("participantes_data.json", "w") as prtc_data_file:
+with open(settings["participantes_data_file"], "w") as prtc_data_file:
     prtc_data_file.write(json.dumps(json_data, indent=4))
 
 print('>> All is Done Here xD')
+print('>> Task finished in: {}'.format(
+    strftime("%H:%M:%S", gmtime(time()-time()+1000))))
